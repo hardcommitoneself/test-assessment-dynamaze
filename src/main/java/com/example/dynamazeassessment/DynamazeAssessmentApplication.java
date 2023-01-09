1bdf62f12dc43c36e2a86b5f8833e1861790bd5b
hardcommitoneself <hardcommitoneself@gmail.com>
2023-01-09 04:08:04 -0800

    init commit

---
+++ src/main/java/com/example/dynamazeassessment/DynamazeAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamazeassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamazeAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamazeAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dynamazeassessment/DynamazeAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamazeassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamazeAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
